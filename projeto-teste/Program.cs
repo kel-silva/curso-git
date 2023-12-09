935a7c1acba7258d51715e34d7e3f7253ead8dc3
kel-silva <kellogical1@gmail.com>
2023-12-09 00:29:45 -0300

    Primeiro projeto

---
+++ projeto-teste/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace projeto_teste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
